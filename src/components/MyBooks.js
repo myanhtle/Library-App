112003fe41483f5264faa3cf2692d396e83203fe
Myanhtle <mtl2mr@virginia.edu>
2021-05-28 16:43:10 -0400

    add search option when library is empty

--- src/components/MyBooks.js
+++ src/components/MyBooks.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import BookCards from "./BookCards";
 import { BookListContext } from "../contexts/bookListContext";
+import Emoji from "react-emoji-render";
 
 export default function MyBooks() {
   const { setBooks } = useContext(BookListContext);
@@ -22,7 +24,22 @@ export default function MyBooks() {
       <div className="p-3" style={{ fontSize: "5vh" }}>
         There are currently {books.length} books in your library
       </div>
-      <BookCards />
+      {books.length === 0 ? (
+        <div style={{ paddingTop: "20vh" }}>
+          <div className="d-flex justify-content-center p-3">
+            <Emoji text="Oh no! :( Your library seems to be empty, let's fix that . . .  " />
+          </div>
+          <div className="d-flex justify-content-center">
+            <div>
+              <Link to="/search" style={{ textDecoration: "none" }}>
+                <Emoji text="Search Now 📚🏃‍♀️" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <BookCards />
+      )}
     </div>
   );
 }
